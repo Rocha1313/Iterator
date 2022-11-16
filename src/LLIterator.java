a9c6f09cbe18c2ba4e5cd8d1b9f7923f63341fe2
rocha <rjoao0112@gmail.com>
2022-11-16 17:01:29 +0000

    Iterator Class Creation

--- src/LLIterator.java
+++ src/LLIterator.java
@@ -2,4 +2,22 @@ import java.util.*;
 
 public class LLIterator<T> implements Iterator<T>{
     Node<T> current;
+
+    public LLIterator(LL<T> list){
+        this.current = list.getHead();
+    }
+
+    public boolean hasNext(){
+        return Objects.isNull(this.current);
+    }
+
+    public T next(){
+        T data = current.getData();
+        current = current.getNext();
+        return data;
+    }
+
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
 }
