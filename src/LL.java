ec932c607c5a70ad4dce019f02a978638b7714c0
rocha <rjoao0112@gmail.com>
2022-11-16 16:57:17 +0000

    Linked List class creation

--- src/LL.java
+++ src/LL.java
@@ -1,5 +1,26 @@
+import java.util.*;
 public class LL<T> implements Iterable<T> {
-    Node<T> head;
+    Node<T> head, tail;
 
+    public void add(T data){
+        Node<T> node = new Node<>(data, null);
+        if(Objects.isNull(head)){
+            tail = head = node;
+            return;
+        }
+        tail.setNext(node);
+        tail = node;
+    }
 
+    public Node<T> getHead(){
+        return this.head;
+    }
+
+    public Node<T> getTail(){
+        return this.tail;
+    }
+
+    public LLIterator<T> iterator(){
+        return new LLIterator<T>(this);
+    }
 }
